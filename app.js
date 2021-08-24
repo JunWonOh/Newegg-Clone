d2c4e1dc0950e047f828e3169ffaa5b7aa36197e
JunWonOh <junoh9847@gmail.com>
2021-08-24 16:41:48 -0400

    added templates

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+const port = 3000;
+
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res){
+    res.render("home")
+})
+
+app.listen(port, function(){
+    console.log("Server initialized on port " + port);
+});
