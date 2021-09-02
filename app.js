3edf21e9bd043b3138411717b48ab115396b18bb
JunWonOh <junoh9847@gmail.com>
2021-09-01 23:34:53 -0400

    added more dependencies

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 //jshint esversion:6
+
+//TEMPORARY, FOR TESTING
 const port = 3000;
 
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema ({
+    email: String, 
+    password: String,
+    googleId: String,
+});
 
 app.get("/", function(req, res){
     res.render("home")
