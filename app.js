14f76eca8c90f2625aa19d6b6c55a89e18df9cb8
JunWonOh <junoh9847@gmail.com>
2021-09-04 01:28:19 -0400

    added more code to support passport.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 //jshint esversion:6
-
+require('dotenv').config();
 //TEMPORARY, FOR TESTING
 const port = 3000;
 
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -21,12 +20,63 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// Level 5
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+// Level 5 - initialize passport, then use it to set up a session
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema ({
     email: String, 
     password: String,
     googleId: String,
+    secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
+
+
+//https://www.passportjs.org/packages/passport-google-oauth20/
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
 });
 
 app.get("/", function(req, res){
