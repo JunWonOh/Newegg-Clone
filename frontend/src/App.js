feab07c72a4cfd9ed747a262cb4c039e9e75c0ab
JunWonOh <junoh9847@gmail.com>
2021-09-13 22:46:47 -0400

    fixed frontend directory

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/home")
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to redload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
