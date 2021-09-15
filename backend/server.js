c4ae1664ef04399685487d8a979a4ffe8ed7152b
JunWonOh <junoh9847@gmail.com>
2021-09-14 20:32:39 -0400

    update directory structure

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
 require('dotenv').config();
 //TEMPORARY, FOR TESTING
-const port = 3000;
+const port = 3001;
 
 const express = require('express');
 const bodyParser = require('body-parser');
